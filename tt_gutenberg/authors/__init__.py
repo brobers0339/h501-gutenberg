2e487719e524f9caba27a4da54ac9d82a9337248
brobers0339 <brobers@iu.edu>
2025-09-23 22:10:21 -0400

    Made some small changes before due date. Exercise 3 is not fully complete, will be revisiting at a later time due to timeline restriction

---
+++ tt_gutenberg/authors/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["retrieve_authors", "retrieve_data"]
+
+from . import retrieve_authors
+from . import retrieve_data
+
---
+++ tt_gutenberg/authors/retrieve_authors.py
@@ -0,0 +1,3 @@
+import pandas as pd
+def retrieve_authors(csv):
+    return pd.read_csv(csv)
